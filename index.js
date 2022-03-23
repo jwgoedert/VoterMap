9602ba8f5a05aa5949c2025cb0f4b75ad22eaf04
James Wesley Goedert <jwgoedert@gmail.com>
2022-03-23 10:38:16 -0500

    Add commenting-remove precision property

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 let width = parseInt(d3.select(".map-box").style("width"));
 let height = width/2;
-
+//You can reassign a value to width here to resize the map to your specs
 let svg = d3.select('.map-box')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
+//Set a projection using the geoAlbersUsa to include Alaska and Hawaii 
+//Scale sets the size, translate positions off the center point of the map 
 let projection = d3.geoAlbersUsa()
-  .precision(0)
+  // .precision(0)
   .scale(height * 2)
   .translate([width / 2, height / 2]);
 
+//Create a geoPath and specify it's projection
 let path = d3.geoPath()
   .projection(projection)
   
-
+//Load Data
 d3.json('./data/us.json', function (error, data) {
 // d3.json("https://d3js.org/us-10m.v1.json", function (error, data) {
 
@@ -29,10 +32,12 @@ d3.json('./data/us.json', function (error, data) {
     .enter()
     .append("path")
     .attr("d", path)
+    
 
   svg.append("path")
     .attr("class", "state-borders")
     .attr("d", path(topojson.mesh(data, data.objects.states, function (a, b) {
       return a != b;
-    })))
-})
+    })));
+
+  })
