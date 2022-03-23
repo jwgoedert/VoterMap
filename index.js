2ab59a64e9f646d605b6e084a7d29ab8dfebe15a
James Wesley Goedert <jwgoedert@gmail.com>
2022-03-23 01:37:00 -0500

    Create working map, link three files together

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-console.log('Voter Map Test');
+let width = parseInt(d3.select(".map-box").style("width"));
+let height = width/2;
 
 let svg = d3.select('.map-box')
-    .append('svg')
-    .attr('width', 100)
-    .attr('height', 100)
-    
-svg.append('rect')
-    .attr('width', 75)
-    .attr('height', 75)
-    .style('fill', 'pink')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
 
-console.log('svg', svg)
+// svg.append('rect')
+//     .attr('width', 960)
+//     .attr('height', 75)
+//     .style('fill', 'pink')
+
+let path = d3.geoPath();
+// let path = d3.geo.path();
+
+// d3.json('./data/us.json', function (error, data) {
+d3.json("https://d3js.org/us-10m.v1.json", function (error, data) {
+
+  if (error) throw error;
+
+  console.log('data', data);
+
+
+  svg.append("g")
+    .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(data, data.objects.states).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+
+  svg.append("path")
+    .attr("class", "state-borders")
+    .attr("d", path(topojson.mesh(data, data.objects.states, function (a, b) {
+      return a != b;
+    })))
+})
