0a3013398ad673611acb16f7a66808190d70de57
James Wesley Goedert <jwgoedert@gmail.com>
2022-03-25 12:40:28 -0500

    Update style sheet and create functions for renderings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let width = parseInt(d3.select(".map-box").style("width"));
 let height = width/2;
 let stateId = 01;
-
+let see = console.log;
 let svg = d3.select('.map-box')
   .append('svg')
   .attr('width', width)
@@ -22,12 +22,14 @@ d3.json("./Data/stateCodes.json", function (error, d) {
   function change() {
     let res = this.options[this.selectedIndex].value;
     let node = d3.select("#selectOptions").node();
-    stateId =  node;
+    stateId =  res;
     console.log(res, node);
   }
 
 });
 
+let query = window.location.search.substring(1);
+console.log(query);
 // let projection = d3.geoEquirectangular()
 let projection = d3.geoAlbersUsa() // d3.geoEquirectangular()
   .precision(0)
@@ -51,10 +53,14 @@ d3.json('./data/us.json', function (error, data) {
     // return statePaths;
   // }
 //filter 
+stateId = parseInt(query);
 
 let states = topojson.feature(data, data.objects.states);
 let counties = topojson.feature(data, data.objects.counties);
-let state = states.features.filter(function (d) { return d.id === stateId;})[0];
+let state = states.features.filter(function (d) { return d.id === stateId; })[0];
+// let stateCounties = counties.features.filter(function (d) { return d.id.toString().slice(0, 2) === stateId.toString(); });
+let stateCounties = counties.features.filter(function (d) { return d.id.toString().slice(0, 2) === stateId.toString(); });
+console.log(stateCounties);
 projection
 .scale(1)
 .translate([0,0])
@@ -67,70 +73,107 @@ projection
 projection 
   .scale(s)
   .translate(t)
-//states
-svg.append("g")
-  .attr("class", "states")
-  .selectAll("path")
-  .data(states.features)
-  .enter()
-  .append("path")
-  .attr("d", path)
-  // .on('click', clicked)
-  
-//state borders
-svg.append("path")
-    .attr("class", "state-borders")
-    .attr("d", path(topojson.mesh(data, data.objects.states, function (a, b) {
-      return a != b;
-    })));
-//county    
-svg.append("g")
-    .attr("class", "counties")
-    .selectAll("path")
-    .data(counties.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-//county borders
-svg.append("g")
-  .attr("class", "county-borders")
-  .selectAll("path")
-  .data(counties.features)
-  .enter()
-  .append("path")
-  .attr("d", path)
-  // .on('click', clicked)
-  
-  
-//  d3.json("./Data/stateCodes.json", function(error, d){
-//       let options = d.states;
-//   options.forEach(function(d, i){
-//     d3.select("#selectOptions")
-//     .append("option")
-//     .attr("value", d.code)
-//     .text(d.state)
-//     .on("change", change)
-//   })
-//   d3.select("#selectOptions")
-//     .on("change", change)
-//   function change() {
-//     let res = this.options[this.selectedIndex].value;
-//     let node = d3.select("#selectOptions").node();
-//     console.log(res, node);
-//   }
 
-// })
+  //states
+  function renderStates() {
+    svg.append("g")
+      .attr("class", "states")
+      .selectAll("path")
+      .data(states.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+    see("state", states.features)
+  }
+  //states borders
+  function renderStatesBorders() {
+    svg.append("path")
+      .attr("class", "states-borders")
+      .attr("d", path(topojson.mesh(data, data.objects.states, function (a, b) {
+        return a != b;
+      })));
+    see('states borders', data.objects.states);
+  }
+  //states counties
+  function renderStatesCounties(){
+    svg.append("g")
+      .attr("class", "states-counties")
+      .selectAll("path")
+      .data(counties.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      see("all counties", counties.features);
+  }    
+  //county borders
+  function renderStatesCountiesBorders(){  
+    svg.append("g")
+      .attr("class", "states-counties-borders")
+      .selectAll("path")
+      .data(counties.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      see("counties borders", counties.features);
+  }  
 
-    // county
-    // svg.append("path")
-    // .attr("class", "county-borders")
-    // .data(counties.features)
-    // // .attr("d", path.topojson.feature(data, data.objects.counties).features)
-    // .attr("d", path(topojson.mesh(data, data.objects.counties, function (a, b) {
-    //   return a != b;
-    // })));
-    // svg.append("path")
-    //   .datum(state)
-    //   .attr("d", path)
+  function renderState() {
+    svg.append("path")
+      .attr("class", "state")
+      .datum(state)
+      .attr("d", path)
+      see('state', state);
+  }
+
+  function renderStateBorders() {
+    svg.append("path")
+      .attr("class", "state-borders")
+      .datum(state)
+      .attr("d", path)
+      see("state borders", state);
+  }
+
+  function renderStateCounties(){
+    svg.append("g")
+      .attr("class", "state-counties")
+      .selectAll("path")
+      .data(stateCounties)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      see("state counties", stateCounties);
+  }
+  
+
+  function renderStateCountiesBorders(){
+    svg.append("g")
+      .attr("class", "state-counties-borders")
+      .selectAll("path")
+      .data(stateCounties)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      see("state counties borders", stateCounties)
+    }
+    renderStates();
+    renderStatesBorders();
+    
+    renderStatesCounties();
+    renderStatesCountiesBorders();
+    
+    renderState();
+    renderStateBorders();
+
+    renderStateCounties();
+    renderStateCountiesBorders();
+
+  // county
+  // svg.append("path")
+  //   .attr("class", "county-borders")
+  //   .data(counties.features)
+  //   // .attr("d", path.topojson.feature(data, data.objects.counties).features)
+  //   .attr("d", path(topojson.mesh(data, data.objects.counties, function (a, b) {
+  //     return a != b;
+  //   })));
 
 })
