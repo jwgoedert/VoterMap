a7d35ba428614efbd317c780659d2c47b59ced9c
James Wesley Goedert <jwgoedert@gmail.com>
2022-04-07 16:46:46 -0500

    restructure files, handle single digit state errors

--- index.js
+++ index.js
@@ -11,18 +11,35 @@ let svg = d3.select('.map-box')
   .attr('height', height)
   
   queue()
-  .defer(d3.json, "./Data/us.json")
-  .defer(d3.csv, "./Data/data.csv")
-  .defer(d3.json, "./Data/stateCodes.json")
+  .defer(d3.json, "./static/data/us.json")
+  .defer(d3.csv, "./static/data/data.csv")
+  .defer(d3.json, "./static/data/stateCodes.json")
   .await(loadData)
-  
+  function fipSan(code, el){
+    code = code.toString();
+    let id, codeLength = code.length;
+      if(codeLength == 5){
+        id = code.slice(0,2);
+      } else if (codeLength == 4){
+        id = `0${code.slice(0,1)}`;
+      } else if (codeLength == 2) {
+        id = code;
+      } else if (codeLength == 1) {
+        id = `0${code}`
+      } else {
+        return console.error(el, code, "invalid state code, codes must be two digits")
+      }
+      return id;
+  }
   function loadData(error, usData, countyData, statesList) {
     if(error) throw error;
-    
+    console.log(countyData);
     d3.select(".state-header")
     .text(statesList.states.find(el => el.code == stateId).state)
     
-    let projection = d3.geoMercator()
+    // let projection = d3.geoMercator()
+    // let projection = d3.geoEquirectangular()
+    let projection = d3.geoAlbers()
     .precision(0)
     .scale(height * 2)
     .translate([width / 2, height / 2]);
@@ -30,11 +47,14 @@ let svg = d3.select('.map-box')
     let path = d3.geoPath()
       .projection(projection);
     
+    
     let states = topojson.feature(usData, usData.objects.states);
     let counties = topojson.feature(usData, usData.objects.counties);
     let state = states.features.filter(function (d) { return d.id === stateId; })[0];
-    let stateCounties = counties.features.filter(function (d) { return d.id.toString().slice(0, 2) === stateId.toString(); });
-    let countyRatings = countyData.filter(function (d) { return d.id.toString().slice(0, 2) === stateId.toString(); });
+    let stateCounties = counties.features.filter(function (d) { return fipSan(d.id, d) == fipSan(stateId.toString()); });
+    let countyRatings = countyData.filter(function (d) { return fipSan(d.id, d) == fipSan(stateId.toString()); });
+    console.log(state, stateCounties, countyRatings, counties);
+    // let countyRatings = countyData.filter(function (d) { return d.id.toString().slice(0, 2) === stateId.toString(); });
     let domainMax = d3.max(countyRatings, function(d){return +d.rate});
     color_domain = d3.range(0, domainMax, domainMax/12);
     let color = d3.scaleThreshold()
