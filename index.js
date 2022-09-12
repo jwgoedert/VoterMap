b384cae6550d5e6d88699b0754c4bd1485f37ceb
James Wesley Goedert <jwgoedert@gmail.com>
2022-09-12 10:25:02 -0500

    Create functioning color shift per button, need to activate percent button

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let stateFromCounty = countyId => countyId.length == 5 ? countyId.slice(0, 2) : 
 let view = ''; 
 let setView;
 let selectView = d3.select("#nav-select-view")
+let color;
 
   let svg = d3.select('.map-box')
   .append('svg')
@@ -50,15 +51,22 @@ function loadData(error, usData, countyData, countyPrev, countyCurrent, countyDr
   let countyRatingsCurrent = countyCurrent.filter(d => stateFromCounty(d.id) == filteredStateId.toString());
   let countyRatingsDrop = countyDrop.filter(d => stateFromCounty(d.id) == filteredStateId.toString());
   let maxCheck = countyRatingsDrop;
-  console.log('maxCheck', maxCheck);
-  let domainMax = d3.max(maxCheck, d => +d.count);
-  // let domainMax = d3.max(countyRatings, d => +d.count);
-  // let domainMax = d3.max(countyRatings, d => +d.rate);
-  console.log('max', domainMax);
-  color_domain = d3.range(0, domainMax, domainMax / 12);
-  let color = d3.scaleThreshold()
-    .domain(color_domain)
-    .range(["#dcdcdc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28"]);
+  // console.log('numbers', d3.max(countyRatingsPrev, d => +d.count), d3.max(countyRatingsCurrent, d => +d.count), d3.max(countyRatingsDrop, d => +d.count));
+  // console.log('maxCheck', maxCheck);
+  let countyByView = county => view.find(el => el.id == county.id);
+  setColor =  function(v){
+    console.log('setcolor',v);
+    let domainMax = d3.max(v || [], d => +d.count);
+    // let domainMax = d3.max(countyRatingsCurrent, d => +d.count);
+    // let domainMax = d3.max(countyRatings, d => +d.rate);
+    console.log('max', domainMax);
+    color_domain = d3.range(0, domainMax, domainMax / 12);
+    console.log('colorDomain', color_domain);
+    color = d3.scaleThreshold()
+      .domain(color_domain)
+      .range(["#dcdcdc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28"]);
+  }
+  // setColor(countyRatingsPrev);
   projection
     .scale(1)
     .translate([0, 0])
@@ -72,7 +80,7 @@ function loadData(error, usData, countyData, countyPrev, countyCurrent, countyDr
     .translate(t)
 
   let countyById = county => countyData.find(el => el.id == county.id);
-  let countyByView = county => view.find(el => el.id == county.id);
+  // let countyByView = county => view.find(el => el.id == county.id);
   let countyByIdCurr = county => countyCurrent.find(el => el.id == county.id);
   let countyByIdPrev = county => countyPrev.find(el => el.id == county.id);
   let countyByIdDrop = county => countyDrop.find(el => el.id == county.id);
@@ -86,10 +94,7 @@ function loadData(error, usData, countyData, countyPrev, countyCurrent, countyDr
     this.parentNode.appendChild(this);
     d3.select(".dash-hover")
       .text(`${countyByIdPrev(d).County}`)
-      .text(`2021 Voter Count: ${countyByIdPrev(d).count}`)
-      .text(`2022 Voter Count: ${countyByIdCurr(d).count}`)
-      .text(`Total Voters Purged: ${countyByIdDrop(d).count}`)
-      .text(`Purged Percentage: ${countyByIdDrop(d).count/countyByIdPrev(d).count}%`)
+      .text(`2021/2022/Dropped: ${countyByIdPrev(d).count}/${countyByIdCurr(d).count}/${countyByIdDrop(d).count}-${countyByIdDrop(d).count/countyByIdPrev(d).count}%`)
       // .text(`${countyByIdPrev(d).County} ${countyByIdDrop(d).count / countyByIdPrev(d).count}`)
       // .text(`${countyByIdPrev(d).County} ${countyByIdDrop(d).count / countyByIdPrev(d).count}`)
       // .text(`${countyById(d).name} ${countyById(d).rate}`);
@@ -103,7 +108,8 @@ function loadData(error, usData, countyData, countyPrev, countyCurrent, countyDr
       .style("stroke", "rgba(13, 106, 106, 0.5)")
   }
 
-  function renderStateCounties() {
+  function renderStateCounties(view) {
+
     svg.append("g")
       .attr("class", "mouse-out")
       .selectAll("path")
@@ -116,27 +122,35 @@ function loadData(error, usData, countyData, countyPrev, countyCurrent, countyDr
       .on("mouseover", mouseOver)
       .on("mouseout", mouseOut)
   }
-  
+  setColor();
   renderStateCounties();
-
   setView = function (v) {
-
     // view = v;
     if (v == "current"){
       view = countyCurrent; 
-      maxCheck = countyRatingsCurrent;
+            // maxCheck = countyRatingsCurrent;
+            setColor(countyRatingsCurrent);
+      // renderStateCounties();
     } else if (v == "previous"){
       view = countyPrev;
-      maxCheck = countyRatingsPrev;
+            // maxCheck = countyRatingsPrev;
+            setColor(countyRatingsPrev);
+      // renderStateCounties();
     } else if (v == "dropped"){
       view = countyDrop;
-      maxCheck = countyRatingsDrop;
+            // maxCheck = countyRatingsDrop;
+            setColor(countyRatingsDrop);
+      renderStateCounties();
     } else if (v == "percentage-dropped"){
       view = countyDrop / countyPrev;
-      maxCheck = countyRatingsDrop/countyRatingsPrev;
+            // maxCheck = countyRatingsDrop/countyRatingsPrev;
+            setColor(countyRatingsDrop/countyRatingsPrev);
+      // renderStateCounties();
     } else {
       view = countyCurrent;
-      maxCheck = countyRatingsPrev;
+            // maxCheck = countyRatingsPrev;
+            setColor(countyRatingsPrev);
+      // renderStateCounties();
     }
     renderStateCounties();
     console.log("view btn", view, v);
