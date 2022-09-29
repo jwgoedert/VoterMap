aef2095787081d0e1c567c3698be0a8813961c2b
James Wesley Goedert <jwgoedert@gmail.com>
2022-09-29 06:11:23 -0500

    Add click for county select, remove hover

--- index.js
+++ index.js
@@ -28,26 +28,31 @@ queue()
 function loadData(error, usData, AllNewCountyData, AllDropCountyData) {
   view = AllDropCountyData;
   console.log('test data', AllDropCountyData, AllNewCountyData);
+  // get state information
   let stateName = usData.objects.counties.geometries.find(el => el.properties.stateCode == stateId).properties.stateName;
   if (error) throw error;
+  // display state information
   d3.select(".state-header")
     .text(stateName);
+  // set projection with Alaska outlier check
   let projection = stateId == 2 ?
     d3.geoAlbers() : d3.geoMercator()
       // let projection = d3.geoEquirectangular()
       .precision(0)
       .scale(height * 2)
       .translate([width / 2, height / 2]);
-
+  // initialize display area
   let path = d3.geoPath()
     .projection(projection);
-
+  // determine state data areas
   let state = topojson.feature(usData, usData.objects.states)
     .features.filter(d => d.id === stateId)[0];
   let stateCounties = topojson.feature(usData, usData.objects.counties)
     .features.filter(d => d.properties.stateCode == filteredStateId);
+  //Calling demographic data`
   let countyDropData = AllDropCountyData.filter(d => stateFromCounty(d.id) == filteredStateId.toString());
   console.log('testing one two', view, countyDropData);
+  // creating color determining function which takes a view as an argument
   setColor = function (v) {
     let domainMax = d3.max(v || [], d => +d.count);
     color_domain = d3.range(0, domainMax, domainMax / 12);
@@ -56,20 +61,24 @@ function loadData(error, usData, AllNewCountyData, AllDropCountyData) {
       .range(["#dcdcdc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28"]);
   };
   // setColor(countyRatingsPrev);
+
+  // set and render content
   projection
     .scale(1)
     .translate([0, 0])
-
+  // set projection boundaries based on state ID
   let b = path.bounds(state),
     s = 1.0 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
     t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
+  // re-render projection  
   projection
     .scale(s)
     .translate(t)
-
+  // create county array
   let countyByView = county => view.find(el => el.id == county.id);
   let countyByIdDropPercentage = county => countyDropData.find(el => el.id == county.id);
+  // determines hover responses
   function mouseOver(d) {
     console.log("CBV", countyByIdDropPercentage(d));
     d3.select(this)
@@ -77,9 +86,21 @@ function loadData(error, usData, AllNewCountyData, AllDropCountyData) {
       .duration(200)
       .style("stroke", "orange")
       .style("stroke-width", 3)
-    this.parentNode.appendChild(this);
-    d3.select(".dash-hover")
-      .text(`${countyByView(d).County} ${countyByView(d).key_pct}% `)
+    // this.parentNode.appendChild(this);
+    // d3.select(".dash-hover")
+    //   .text(`${countyByView(d).County} ${countyByView(d).key_pct}% `)
+  }
+
+  function click(d) {
+    // selectedCounty = countyById(countyData, d);
+    d3.selectAll("path")
+      .style("fill", d => color(countyByView(d)) ? color(countyByView(d).count) : "white")
+      // .style("fill", null);
+    // renderStateCounties();
+    d3.select(this)
+      .style("fill", "orange");
+    d3.select(".selected")
+      .text(`Selected: ${ countyByView(d).County } ${ countyByView(d).key_pct } % `);
   }
 
   function mouseOut(d) {
@@ -89,7 +110,7 @@ function loadData(error, usData, AllNewCountyData, AllDropCountyData) {
       .style("stroke-width", .5)
       .style("stroke", "rgba(13, 106, 106, 0.5)")
   }
-
+  // after mouseOute
   function renderStateCounties(view) {
 
     svg.append("g")
@@ -103,26 +124,33 @@ function loadData(error, usData, AllNewCountyData, AllDropCountyData) {
       // .style("fill", d => color(countyById(d)) ? color(countyById(d).rate) : "white")
       .on("mouseover", mouseOver)
       .on("mouseout", mouseOut)
+      .on("click", click)
   }
   setColor(countyDropData);
   renderStateCounties();
-  setView = function (v) {
-    if (v == "previous") {
-      view = countyDropData;
-      setColor(countyDropData);
-      renderStateCounties();
-    } else if (v == "percentage-dropped") {
-      view = countyDropData;
-      setColor(countyDropData);
-      renderStateCounties();
-    } else {
-      view = countyPrev;
-      setColor(countyRatingsPrev);
-      renderStateCounties();
-    }
-    renderStateCounties();
-  };
-  selectView
+  
+  
+  // setView = function (v) {
+  //   if (v == "previous") {
+  //     view = countyDropData;
+  //     setColor(countyDropData);
+  //     renderStateCounties();
+  //   } else if (v == "percentage-dropped") {
+  //     view = countyDropData;
+  //     setColor(countyDropData);
+  //     renderStateCounties();
+  //   } else {
+  //     view = countyPrev;
+  //     setColor(countyRatingsPrev);
+  //     renderStateCounties();
+  //   }
+  //   renderStateCounties();
+  // };
+
+
+
+
+  // selectView
   //   .append("input")
   //   .attr("class", "button")
   //   .attr("type", "button")
